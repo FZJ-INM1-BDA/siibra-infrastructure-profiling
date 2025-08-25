eec802e03062b9b9285d3d7121a82c9b30ef0d63
Xiao Gui <xgui3783@gmail.com>
2025-08-25 14:40:47 +0200

    rerun cdn

--- scripts/run_geoprofile_cdn.sh
+++ scripts/run_geoprofile_cdn.sh
@@ -5,33 +5,33 @@ run_num=${run_num:-0}
 
 IFS=","
 LOCUST_FILENAME="conf/bigbrain_cdn.txt"
-# for region in $regions
-# do
-#     echo $region
-#     LOCUST_TARGET="cdn-data-vm"
+for region in $regions
+do
+    echo $region
+    LOCUST_TARGET="cdn-data-vm"
     
-#     ansible-playbook -i ansible/inventory.yml \
-#         -e TARGET_DIR=../../run-result/geoprofile-datavm-cdn-$run_num/ \
-#         -e LOCUST_TARGET=$LOCUST_TARGET \
-#         -e LOCUST_FILENAME=$LOCUST_FILENAME \
-#         -e LINODE_TOKEN=$LINODE_TOKEN \
-#         -e LINODE_REGION=$region \
-#         ansible/playbooks/workflow.profile.playbook.yml
-# done
+    ansible-playbook -i ansible/inventory.yml \
+        -e TARGET_DIR=../../run-result/geoprofile-datavm-cdn-$run_num/ \
+        -e LOCUST_TARGET=$LOCUST_TARGET \
+        -e LOCUST_FILENAME=$LOCUST_FILENAME \
+        -e LINODE_TOKEN=$LINODE_TOKEN \
+        -e LINODE_REGION=$region \
+        ansible/playbooks/workflow.profile.playbook.yml
+done
 
-# for region in $regions
-# do
-#     echo $region
-#     LOCUST_TARGET="cdn-siibra-api"
+for region in $regions
+do
+    echo $region
+    LOCUST_TARGET="cdn-siibra-api"
     
-#     ansible-playbook -i ansible/inventory.yml \
-#         -e TARGET_DIR=../../run-result/geoprofile-siibraapi-cdn-$run_num/ \
-#         -e LOCUST_TARGET=$LOCUST_TARGET \
-#         -e LOCUST_FILENAME=$LOCUST_FILENAME \
-#         -e LINODE_TOKEN=$LINODE_TOKEN \
-#         -e LINODE_REGION=$region \
-#         ansible/playbooks/workflow.profile.playbook.yml
-# done
+    ansible-playbook -i ansible/inventory.yml \
+        -e TARGET_DIR=../../run-result/geoprofile-siibraapi-cdn-$run_num/ \
+        -e LOCUST_TARGET=$LOCUST_TARGET \
+        -e LOCUST_FILENAME=$LOCUST_FILENAME \
+        -e LINODE_TOKEN=$LINODE_TOKEN \
+        -e LINODE_REGION=$region \
+        ansible/playbooks/workflow.profile.playbook.yml
+done
 
 for region in $regions
 do
