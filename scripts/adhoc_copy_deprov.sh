84f607378d59ea1937d5a31ca8b3b5604760e868
Xiao Gui <xgui3783@gmail.com>
2025-09-15 11:20:11 +0200

    geoprofile + guided tour

---
+++ scripts/adhoc_copy_deprov.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+if [[ -z "$LINODE_TOKEN" ]]
+then
+    echo "LINODE_TOKEN env var must be defined"
+    exit 1
+fi
+
+if [[ -z "$LINODE_IP" ]]
+then
+    echo "LINODE_IP must be defined"
+    exit 1
+fi
+
+if [[ -z "$LINODE_REGION" ]]
+then
+    echo "LINODE_REGION must be defined"
+    exit 1
+fi
+
+if [[ -z "$LINODE_ID" ]]
+then
+    echo "LINODE_ID must be defined"
+    exit 1
+fi
+
+if [[ -z "$TARGET_DIR" ]]
+then
+    echo "TARGET_DIR must be defined"
+    exit 1
+fi
+
+
+yml='
+- name: "Set variable"
+  hosts: localhost
+
+  tasks:
+  - name: Add remote VM
+    add_host:
+      hostname: "'$LINODE_IP'"
+      groups: remotevms
+    changed_when: false
+    
+  - name: Set facts
+    set_fact:
+    
+      LINODE_TOKEN: '$LINODE_TOKEN'
+      LINODE_REGION: "'$LINODE_REGION'"
+      LINODE_IDS: 
+      - "'$LINODE_ID'"
+      LINODE_IDS_IPV4S:
+        "'$LINODE_ID'": "'$LINODE_IP'"
+      LINODE_IDS_REGION:
+        "'$LINODE_ID'": "'$LINODE_REGION'"
+      TARGET_DIR: "'$TARGET_DIR'"
+
+- name: "Copy result"
+  import_playbook: "./ansible/playbooks/copyresult.playbook.yml"
+
+- name: "Deprov"
+  import_playbook: "./ansible/playbooks/deprovnode.playbook.yml"
+'
+
+ansible-playbook -i ansible/inventory.yml /dev/stdin << EOF
+$yml
+EOF
